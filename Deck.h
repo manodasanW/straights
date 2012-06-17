449993c816903950f1501521ffa3235e12ed3537
Nicholas Kim <foehammer.nickel@gmail.com>
2012-06-17 02:39:35 +0000

    implemented Deck class

--- Deck.h
+++ Deck.h
@@ -1,15 +1,31 @@
+/*
+ Deck.h
+ 
+ Declaration of deck class.
+ */
+
+#ifndef _DECK_H_
+#define _DECK_H_
+
+#include <ostream>
+#include <vector>
+
+#include "Card.h"
+
 class Deck {
 	
 	public:
 		Deck();
 		~Deck();
 		void shuffle();
-		vector<const Card*> dealPlayerHand();
-		friend std::ostream &operator<<(std::ostream &, Deck &);
+        std::vector<const Card*> dealPlayerHand();
+		friend std::ostream &operator<<(std::ostream &, const Deck &);
 
 	private:
-		vector<const Card*> cards;
-
+        int cards_left_;
+        std::vector<const Card*> cards_;
 };
 
 std::ostream &operator<<(std::ostream &, Deck &);
+
+#endif /* _DECK_H_ */
