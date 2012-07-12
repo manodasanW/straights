9b9ffe08337aced64f0cbf5c2935aaae52f2a155
Nicholas Kim <foehammer.nickel@gmail.com>
2012-07-12 02:10:01 +0000

    started work on control frame

--- ControlFrame.cpp
+++ ControlFrame.cpp
@@ -1,5 +1,21 @@
 #include "ControlFrame.h"
 
-ControlFrame::ControlFrame(){}
+ControlFrame::ControlFrame() : controlArea(false, 10), mainControl(true, 10),
+    startButton("Start Game"), endButton("End Game"), seedEntryControl(false, 10),
+    seedEntryLabel(new Gtk::Label("Seed:"))
+{
+    mainControl.add(startButton);
+    mainControl.add(endButton);
+    
+    seedEntry.set_max_length(9);
+    //seedEntryControl.add(seedEntryLabel);
+    seedEntryControl.add(seedEntry);
+    
+    controlArea.add(mainControl);
+    controlArea.add(seedEntryControl);
+    add(controlArea);
+}
 
-ControlFrame::~ControlFrame(){}
+ControlFrame::~ControlFrame() {
+    //delete seedEntryLabel;
+}
--- ControlFrame.h
+++ ControlFrame.h
@@ -1,17 +1,24 @@
 #ifndef CONTROL_FRAME
 #define CONTROL_FRAME
 
-#include <gtkmm/frame.h>
+#include <gtkmm.h>
 
 class ControlFrame : public Gtk::Frame {
 
 public:
 	ControlFrame();
-	~ControlFrame();
+	virtual ~ControlFrame();
 
 private:
-	
-
+    Gtk::VBox controlArea;
+    
+    Gtk::HBox mainControl;
+    Gtk::Button startButton;
+    Gtk::Button endButton;
+    
+    Gtk::HBox seedEntryControl;
+    Gtk::Label *seedEntryLabel;
+    Gtk::Entry seedEntry;
 };
 
 #endif
